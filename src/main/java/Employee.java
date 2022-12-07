f32785ec1d0a05ce90e150a69637bc408cf078b2
Andrew Peterson <andrew-peterson@hotmail.com>
2022-12-07 10:22:25 +0000

    Added employee info prompt

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Employee {
+    public static void main(String[] args){
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter Employee Name: ");
+        String EmployName = name.nextLine();
+
+        Scanner Surname = new Scanner(System.in);
+        System.out.println("Enter Employee Surname: ");
+        String EmploySurname = Surname.nextLine();
+
+        Scanner EmployeeNumber = new Scanner(System.in);
+        System.out.println("Enter Employee Number: ");
+        String EmployNo = EmployeeNumber.nextLine();
+
+        System.out.println("Employee Details are: ");
+    }
+
 }
